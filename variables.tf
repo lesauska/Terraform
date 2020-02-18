98f0351e3a77910cca26f7dc8df13dad002c44d1
lesauska <41114600+lesauska@users.noreply.github.com>
2020-02-17 20:30:23 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "instance_name" {
   description = "Used to populate the Name tag. This is done in main.tf"
 }
 
-variable "instance_type" {}
+variable "instance_type" {T2}
 
 variable "subnet_id" {
   description = "The VPC subnet the instance(s) will go in"
@@ -30,6 +30,6 @@ variable "tags" {
 }
 
 // Variables for providers used in this module
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_region" {}
+variable "aws_access_key" {AKIAIHUP4U4VBKDLBTIQ}
+variable "aws_secret_key" {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}
+variable "aws_region" {us-east-2}
